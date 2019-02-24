fc474bc832bc31a089f0c8568b1f74c29451890b
jacob-hochstrasser <35405376+jacob-hochstrasser@users.noreply.github.com>
2019-02-24 13:09:36 -0500

    Code cleanup
    Added comments. Cleaned up any sloppy/unused code.

--- Project/src/ca/mcgill/ecse211/project/Identifier.java
+++ Project/src/ca/mcgill/ecse211/project/Identifier.java
@@ -5,6 +5,14 @@ import lejos.robotics.SampleProvider;
 
 public class Identifier {
 
+    /*<-------------------------------- Static Variables ------------------------------------------>*/
+    /*<------------------------------------ Public ------------------------------------------------>*/
+    /**
+     * An enumeration of the different target colors that may be chosen. Includes methods for return a string 
+     * of the name of the option chosen and for decoding an integer value to return the corresponding option.
+     * @author jacob
+     *
+     */
     public static enum TARGET_COLOR {
         Any,
         Blue, 
@@ -56,17 +64,31 @@ public class Identifier {
             return value;
         }
     }
+    /**
+     * The distance the robot stops from the can to start scanning it
+     */
+    public static final float SCANNING_DISTANCE = 2.5f;
     
-    public static final float samplingDistance = 2.5f;
+    /*<----------------------------------- Private ------------------------------------------------->*/
+    /**
+     * 
+     */
+    private static final float[][] LI_REFERENCE_STATS = {{0.25f, 0.25f, 0.75f}, {0.25f, 0.75f, 0.25f}, {0.75f, 0.75f, 0.25f}, {0.75f, 0.25f, 0.25f}};
+    
+    /*<------------------------------------ Instance variables ------------------------------------->*/
     private final TARGET_COLOR target;
     private final SampleProvider idLSRGB;
     private float[] idLSData;
     private int numSamples;
     private boolean isSampling;
-    
-    private static final float[][] LI_REFERENCE_STATS = {{0.25f, 0.25f, 0.75f}, {0.25f, 0.75f, 0.25f}, {0.75f, 0.75f, 0.25f}, {0.75f, 0.25f, 0.25f}};
     private float [][] samples;
     
+    /**
+     * Creates an Identifier object with the specified parameters
+     * @param target - The target color {1 - Blue, 2 - Green, 3 - Yellow, 4 - Red}
+     * @param idLSRGB - The sample provider for the light sensor (RGB mode)
+     * @param numSamples - The number of samples taken when scanning
+     */
     public Identifier (int target, SampleProvider idLSRGB, int numSamples) {
         this.target = TARGET_COLOR.decodeValue(target);
         this.idLSRGB = idLSRGB;
@@ -80,11 +102,11 @@ public class Identifier {
         samples = new float[numSamples][3];
     }
     
-//    public void run() {
-//        
-//    }
-    
-    public boolean identifyCan() {
+    /**
+     * Scans the current can a determines if it is the target can or not
+     * @return boolean representing whether a target was found
+     */
+    public boolean isTargetCan() {
         this.isSampling = true;
         for(int i = 0; i < numSamples; i++) {
             idLSRGB.fetchSample(idLSData, 0);
@@ -92,6 +114,11 @@ public class Identifier {
         }
         return true;
     }
+    
+    /**
+     * Changes the number of samples taken during scanning
+     * @param newNumSamples
+     */
     public void setNumSamples(int newNumSamples) {
         if(!isSampling &&(newNumSamples%4 == 0)) {
             this.numSamples = newNumSamples;
