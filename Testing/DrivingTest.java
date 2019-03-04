36522b1e498f02795aac5011d1e1acba8eb2f604
Cytosine <35353368+Catosine@users.noreply.github.com>
2019-03-04 12:49:10 -0500

    Add files via upload

--- Testing/DrivingTest.java
+++ Testing/DrivingTest.java
@@ -1,19 +1,35 @@
 package ca.mcgill.ecse211.testing;
 
-import lejos.hardware.ev3.LocalEV3;
-import lejos.hardware.motor.EV3LargeRegulatedMotor;
-
 public class DrivingTest {
+	/**
+	 * This class is used for driving tests.
+	 * 
+	 * @author Pengnan Fan
+	 */
+	
+	//-----<Important Constant>-----//
 	private static final double TILE_SIZE = 30.48;//cm
+	
 	public static void main(String[] args) {
 		MotorController mc = MotorController.getMotorController("B", "D");
 		
 		//Square Drive
+		squareDriver(mc);
+	}
+	
+	/**
+	 * @author Pengnan Fan
+	 * 
+	 * This method drives the robot to move as a 3*3 square.
+	 * 
+	 * @param mc:MotorController
+	 * This input is the MotorController.
+	 */
+	public static void squareDriver(MotorController mc) {
 		for(int i = 0; i<4; i++) {
 			mc.moveForward(TILE_SIZE*3);
 			mc.turnLeft(90);
 		}
 	}
-	
 	
 }
--- Testing/MotorController.java
+++ Testing/MotorController.java
@@ -4,9 +4,17 @@ import lejos.hardware.ev3.LocalEV3;
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
 
 public class MotorController {
+	
+	/** 
+	 * This is a class that controlls motors. Many of the movements could be find here.
+	 * 
+	 * @author Pengnan Fan
+	 *
+	 */
+	
 	//-----<Important Constant>-----//
-	private static final double RADIUS = 1.58;//cm
-	private static final double TRACK = 15;//cm
+	private static final double RADIUS = 1.58;//cm. It is certifed by testing from Pengnan.
+	private static final double TRACK = 15;//cm. It is certified by testing from Pengnan.
 	volatile private static int ACCELERATION = 500;
 	volatile private static int TEST_SPEED = 150;
 	private static final int TIME_INTERVAL = 15;//ms
@@ -18,6 +26,22 @@ public class MotorController {
 	//-----<Nested MotorController>-----//
 	private static MotorController mc;
 	
+	/**
+	 * @author Pengnan Fan
+	 * 
+	 * This is a static method used to get a MotorController object.
+	 * Note: Only one of this type of object can be accessed.
+	 * 
+	 * @param leftMotor:String
+	 * This input shows the port used for left motor.
+	 * 
+	 * @param rightMotor:String
+	 * This input shows the port used for right motor.
+	 * 
+	 * @return mc:MotorController
+	 * This output is the MotorController nested in the class.
+	 * 
+	 */
 	public static MotorController getMotorController(String leftMotor, String rightMotor) {
 		if(mc==null) {
 			mc = new MotorController(leftMotor, rightMotor);
@@ -32,23 +56,53 @@ public class MotorController {
 		initializeSpeed();
 	}
 	
+	/**
+	 * @author Pengnan Fan
+	 * 
+	 * This method reset the acceleration and re-initialize the motor.
+	 * 
+	 * @param newAcceleration:int
+	 * This input is the new acceleration.
+	 * 
+	 */
 	public void setAcceleration(int newAcceleration) {
 		ACCELERATION = newAcceleration;
 		initializeAcceleration();
 	}
 	
+	/**
+	 * @author Pengnan Fan
+	 * 
+	 * This method reset the speed and re-initialize the motor.
+	 * 
+	 * @param newSpeed:int
+	 * This input is the new speed.
+	 * 
+	 */
 	public void setSpeed(int newSpeed) {
 		TEST_SPEED = newSpeed;
 		initializeSpeed();
 	}
 	
+	/**
+	 * @author Pengnan Fan
+	 * 
+	 * This method initialize the speed of motors.
+	 * 
+	 */
 	public void initializeSpeed() {
 		synchronized(MotorController.class) {
 			LEFT_MOTOR.setSpeed(TEST_SPEED);
 			RIGHT_MOTOR.setSpeed(TEST_SPEED);
 		}
 	}
-	
+
+	/**
+	 * @author Pengnan Fan
+	 * 
+	 * This method initialize the acceleration of motors.
+	 * 
+	 */
 	public void initializeAcceleration() {
 		synchronized(MotorController.class) {
 			LEFT_MOTOR.setAcceleration(ACCELERATION);
@@ -56,7 +110,16 @@ public class MotorController {
 		}
 	}
 	
-	public void moveBackword(double distance) {
+	/**
+	 * @author Pengnan Fan
+	 * 
+	 * This method drives the robot backward to a given distance. It will automatically call moveForward if the distance is negative
+	 * 
+	 * @param distance:int
+	 * This input shows how much the robot should move backward in centimeter.
+	 * 
+	 */
+	public void moveBackward(double distance) {
 		if(distance<0) {moveForward(distance);}
 			
 		synchronized(MotorController.class) {
@@ -67,8 +130,17 @@ public class MotorController {
 		try {Thread.sleep(TIME_INTERVAL);} catch (InterruptedException e) {}
 	}
 	
+	/**
+	 * @author Pengnan Fan
+	 * 
+	 * This method drives the robot forward to a given distance. It will automatically call moveBackward if the distance is negative.
+	 * 
+	 * @param distance:int
+	 * This input shows how much the robot should move forward in centimeter.
+	 * 
+	 */
 	public void moveForward(double distance) {
-		if(distance<0) {moveBackword(distance);}
+		if(distance<0) {moveBackward(distance);}
 		
 		synchronized(MotorController.class) {
 			LEFT_MOTOR.rotate(convertDistance(distance), true);
@@ -78,6 +150,15 @@ public class MotorController {
 		try {Thread.sleep(TIME_INTERVAL);} catch (InterruptedException e) {}
 	}
 	
+	/**
+	 * @author Pengnan Fan
+	 * 
+	 * This method turns the robot clockwise to a given degree. It will automatically call turnRight if the input degree is negative.
+	 * 
+	 * @param dTheta
+	 * This input shows how much the robot should turn clockwise in degrees.
+	 * 
+	 */
 	public void turnLeft(double dTheta) {
 		if (dTheta<0) {
 			turnRight(-dTheta);
@@ -90,7 +171,16 @@ public class MotorController {
 		
 		try {Thread.sleep(TIME_INTERVAL);} catch (InterruptedException e) {}
 	}
-	  
+	
+	/**
+	 * @author Pengnan Fan
+	 * 
+	 * This method turns the robot counter clockwise to a given degree. It will automatically call turnLeft if the input degree is negative.
+	 * 
+	 * @param dTheta
+	 * This input shows how much the robot should turn counter clockwise in degrees.
+	 * 
+	 */
 	public void turnRight(double dTheta) {
 		if (dTheta<0) {
 		    turnLeft(-dTheta);
@@ -104,6 +194,11 @@ public class MotorController {
 		try {Thread.sleep(TIME_INTERVAL);} catch (InterruptedException e) {}
 	}
 	
+	/**
+	 * @author Pengnan Fan
+	 * 
+	 * This method will stop the robot immediately.
+	 */
 	public void stop() {
 		synchronized(MotorController.class) {
 			LEFT_MOTOR.stop(true);
