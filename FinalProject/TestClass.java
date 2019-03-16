d9cd5a3fea428fcee16a311bf3f25176144ca8e1
Cytosine <35353368+Catosine@users.noreply.github.com>
2019-03-16 13:20:55 -0400

    Add files via upload

---
+++ FinalProject/TestClass.java
@@ -0,0 +1,8 @@
+package ca.mcgill.ecse211.FinalProject;
+
+public class TestClass {
+	public static void main(String[] args) {
+		Navigation nv = Navigation.getNavigation("B", "D");
+		nv.demo();
+	}
+}
