bdae55dabd6e66d9b443fd4dca1f6b6a3f35dd65
Cytosine <35353368+Catosine@users.noreply.github.com>
2019-03-19 20:47:39 -0400

    Add files via upload

--- FinalProject/src/ca/mcgill/ecse211/Project/Navigation.java
+++ FinalProject/src/ca/mcgill/ecse211/Project/Navigation.java
@@ -286,12 +286,16 @@ public class Navigation {
 						turnTo(calculateTheta((row+LL[1])*TILE_SIZE - position[0], (col+LL[0])*TILE_SIZE - position[1]));
 						LEFT_MOTOR.forward();
 						RIGHT_MOTOR.forward();
-						if (can_dis < CAN_DETECTION) {
+						if (can_dis < CAN_DETECTION&&!foundCan) {
 							stop();
 							turnLeft(180);
 							moveBackward(CAN_DETECTION);
 							//TODO Start measuring color
 							foundCan = ID.isTargetCan();
+							if (foundCan) {
+								travelTo(UR[0], UR[1]);
+								return;
+							}
 							moveForward(CAN_DETECTION);
 							turnLeft(180);
 						}
@@ -306,12 +310,16 @@ public class Navigation {
 					turnTo(calculateTheta(UR[0]*TILE_SIZE - position[0], (row+LL[1])*TILE_SIZE - position[1]));
 					LEFT_MOTOR.forward();
 					RIGHT_MOTOR.forward();
-					if (can_dis < CAN_DETECTION) {
+					if (can_dis < CAN_DETECTION&&!foundCan) {
 						stop();
 						turnLeft(180);
 						moveBackward(CAN_DETECTION);
 						//TODO Start measuring color
 						foundCan = ID.isTargetCan();
+						if (foundCan) {
+							travelTo(UR[0], UR[1]);
+							return;
+						}
 						moveForward(CAN_DETECTION);
 						turnLeft(180);
 					}
@@ -325,12 +333,16 @@ public class Navigation {
 						turnTo(calculateTheta((row+LL[1])*TILE_SIZE - position[0], (col+LL[0])*TILE_SIZE - position[1]));
 						LEFT_MOTOR.forward();
 						RIGHT_MOTOR.forward();
-						if (can_dis < CAN_DETECTION) {
+						if (can_dis < CAN_DETECTION&&!foundCan) {
 							stop();
 							turnLeft(180);
 							moveBackward(CAN_DETECTION);
 							//TODO Start measuring color
 							foundCan = ID.isTargetCan();
+							if (foundCan) {
+								travelTo(UR[0], UR[1]);
+								return;
+							}
 							moveForward(CAN_DETECTION);
 							turnLeft(180);
 						}
@@ -344,12 +356,16 @@ public class Navigation {
 					turnTo(calculateTheta(LL[0]*TILE_SIZE - position[0], (row+LL[1])*TILE_SIZE - position[1]));
 					LEFT_MOTOR.forward();
 					RIGHT_MOTOR.forward();
-					if (can_dis < CAN_DETECTION) {
+					if (can_dis < CAN_DETECTION&&!foundCan) {
 						stop();
 						turnLeft(180);
 						moveBackward(CAN_DETECTION);
 						//TODO Start measuring color
 						foundCan = ID.isTargetCan();
+						if (foundCan) {
+							travelTo(UR[0], UR[1]);
+							return;
+						}
 						moveForward(CAN_DETECTION);
 						turnLeft(180);
 					}
